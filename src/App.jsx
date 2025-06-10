27addae8adc9b345e4c5d3f0c7237998b640e550
karthikcrazy4 <karthikeyasiripragada4@gmail.com>
2025-06-10 15:15:22 +0530

    update src files to use new API endpoints

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useState, createContext } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -6,25 +5,26 @@ import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
 import Login from "./Login";
-import Cart from "./cart";
+import Cart from "./Cart";
 import Register from "./Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 export const AppContext = createContext();
 function App() {
   const [users,setUsers] = useState([])
-  const [cart, setCart] = useState([]);
+  const [cart,setCart] = useState({})
+  const [email,setEmail] = useState()
   return (
     <div>
-      <AppContext.Provider value={{users,setUsers, cart, setCart}}>
+      <AppContext.Provider value={{users,setUsers,cart,setCart,email,setEmail}}>
         <BrowserRouter>
           <Header name="mu-react-store" />
           <Routes>
             <Route index element={<Home />} />
             <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/cart" element={<h1>cart</h1>} />
           </Routes>
           <Footer />
         </BrowserRouter>
