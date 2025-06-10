28c2514367cb463be94f286ac63607e166d6baee
karthikcrazy4 <karthikeyasiripragada4@gmail.com>
2025-06-10 13:00:23 +0530

    jhkhjlb

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,11 @@
-
 import React from "react";
 import "./Header.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 export default function Header({ name }) {
+  const navigate = useNavigate();
+  const handleCartClick = () => {
+    navigate("/buynow");
+  };
   return (
     <div className="App-Header-Row" style={{ backgroundColor: "pink" }}>
       <div>
@@ -15,6 +18,19 @@ export default function Header({ name }) {
           </li>
           <li>
             <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <button
+              onClick={handleCartClick}
+              style={{
+                background: "none",
+                border: "none",
+                padding: 0,
+                cursor: "pointer",
+              }}
+            >
+              <h1>cart</h1>
+            </button>
           </li>
         </ul>
       </div>
