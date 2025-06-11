9a429a6612f5d5fcff7036abf5d9c4147d3a52fc
karthikcrazy4 <karthikeyasiripragada4@gmail.com>
2025-06-11 10:36:44 +0530

    uodate

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
-
-export default function Products() {
-  return (
-    <div>Products</div>
+import React, { useEffect } from 'react' 
+import { usestate } from 'react'
+const [count,SetCount] = useState(0);
+  const fetchproducts = () => {
+    alert("Fetching products from API");
+  };
+  useeffect(() => {fetchproducts()},[]) 
+  return(
+    <div>
+      {count}
+      <button onClick={() => SetCount(count + 1)}>Update count</button>
+    </div>
   )
-}
