93226df1718f622c656c8e330675524e95e0e189
karthikcrazy4 <karthikeyasiripragada4@gmail.com>
2025-06-12 15:33:49 +0530

    update README with new features and bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useState, createContext } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -10,8 +9,8 @@ import Cart from "./Cart";
 import Logout from "./Logout";
 import Order from "./Order";
 import Register from "./Register";
-import Product from "./Product";
 import Test from "./Test";
+import Product from "./Product";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 export const AppContext = createContext();
@@ -28,9 +27,8 @@ function App() {
         <BrowserRouter>
           <Header name="mu-react-store" />
           <Routes>
-            <Route index element={<Product />} />
             <Route index element={<Test />} />
-            <Route path="/" element={<Product />} />
+            <Route path="/" element={<Test />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/login" element={<Login />} />
             <Route path="/logout" element={<Logout />} />
