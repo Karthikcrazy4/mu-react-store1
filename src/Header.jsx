54b96c76c78a7d40192c0ce716ee290f5f05c1d2
karthikcrazy4 <karthikeyasiripragada4@gmail.com>
2025-06-14 10:46:04 +0530

    Initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AppContext } from "./App";
+export default function Header({ name }) {
+  const { email } = useContext(AppContext);
+  return (
+    <div className="App-Header-Row" style={{ backgroundColor: "pink" }}>
+      <div>
+        <h1>{name}</h1>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+
+          <li>
+            {email ? (
+              <>
+                <Link to="/order">Order</Link>-
+                <Link to="/logout">Logout</Link>
+              </>
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ src/Register.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useContext } from "react";
+import { AppContext } from "./App";
+import { useRef } from "react";
+export default function Register() {
+  const [user, setUser] = useState({});
+  const Navigate = useNavigate();
+  const { users, setUsers } = useContext(AppContext);
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passRef = useRef();
+  const handleSubmit = () => {
+    const userObj = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      pass: passRef.current.value,
+    };
+    // setUsers([...users, user]);
+    setUsers([...users, userObj]);
+    Navigate("/login");
+  };
+  return (
+    <div>
+      <h2>Register</h2>
+      <p>
+        <input
+          type="text"
+          placeholder="Enter Name"
+          ref={nameRef}
+          // onChange={(e) => setUser({ ...user, name: e.target.value })}
+        />
+      </p>
+      <p>
+        <input
+          type="text"
+          ref={emailRef}
+          // onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="Enter Email Address"
+        />
+      </p>
+      <p>
+        <input
+          type="password"
+          ref={passRef}
+          // onChange={(e) => setUser({ ...user, pass: e.target.value })}
+          placeholder="New Password"
+        />
+      </p>
+      <p>
+        <button onClick={handleSubmit}>Submit</button>
+      </p>
+      <hr />
+      <p>
+        <Link to="/login">Aready a member? Login Here...</Link>
+      </p>
+    </div>
+  );
+}
